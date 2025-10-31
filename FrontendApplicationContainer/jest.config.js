18fb36740acf4819d928e9cdb42cca3a3c8e69ac
Kavia AI <production@kavia.ai>
2025-10-31 05:57:41 +0000

    CheckPoint - cge171d87f

--- FrontendApplicationContainer/jest.config.js
+++ FrontendApplicationContainer/jest.config.js
@@ -5,11 +5,18 @@ module.exports = {
   // Ensure setupTests.js is executed after the environment is set up
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
 
-  // Allow transforming axios even if it's in node_modules, to handle ESM/CJS interop
-  transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
-
   // Map axios to its CJS build to avoid ESM import issues with Jest
   moduleNameMapper: {
     '^axios$': '<rootDir>/node_modules/axios/dist/node/axios.cjs'
-  }
+  },
+
+  // Use babel-jest to transform JS/TS files so ESM/CJS interop works in Jest
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest'
+  },
+
+  // Allow transforming axios even if it's in node_modules, to handle ESM/CJS interop
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios)/)'
+  ]
 };
